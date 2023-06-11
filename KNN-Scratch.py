484dc223ad7890704936a610af7f24d0e3d72e56
aneesh-sathe <117112887+aneesh-sathe@users.noreply.github.com>
2023-06-12 01:04:23 +0530

    updated KNN

--- KNN-Scratch.py
+++ KNN-Scratch.py
@@ -9,7 +9,6 @@ X = df.iloc[:, :8]
 Y = df.iloc[:, -1:]
 
 # Cleaning the Data
-
 for column in X.columns[1:]:
     X[column] = X[column].replace(0, np.NaN)
     replace_val = int(X[column].mean(skipna=True))
@@ -27,25 +26,31 @@ for column in X.columns:
     sigma = X[column].std()
     X[column] = X[column].apply(lambda x: zNorm(x, mu, sigma))
 
+
 X['Label'] = Y
 X = X.to_numpy()
+
+# Distance Function
 
 
 def euclidDist(p, q):
     return np.sqrt(np.sum((np.array(p) - np.array(q)) ** 2))
+
+# KNN Classifier
 
 
 def KNN(trainData, testData, k=3):
     distances = []
     for i in trainData:
         d = euclidDist(i, testData)
-        distances.append((i, d))
-    distances = sorted(distances)
-    neighbours = distances[:k][0][-1]
-    counts = Counter(neighbours)
-    prediction = max(counts, key=counts.get)
+        distances.append([i, d])
+    distances = sorted(distances, key=lambda x: x[1])
+    neighbours = [distances[:k][0][0][-1]]
+    prediction = Counter(neighbours).most_common(1)[0][0]
     return prediction
 
 
-r = KNN(X, [1, 89, 66, 23, 94, 28.1, 0.167, 21, 0])
-print(r)
+predictedLabel = KNN(X, [0.63953049, 0.86469014, -0.0319691,  0.67020577, -0.00330798, 0.16713124,
+                         0.46818687,  1.42506672, 1])
+
+print(predictedLabel)
