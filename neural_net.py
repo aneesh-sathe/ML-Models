790d0b969fb0fb5dd017aa6892d87fdf3c97b9cb
aneesh-sathe <117112887+aneesh-sathe@users.noreply.github.com>
2023-08-29 00:17:10 +0800

    train

--- neural_net.py
+++ neural_net.py
@@ -15,10 +15,6 @@ def init_params(layers_conf):  # initialise the weight and bias matrices based o
     return layers
 
 
-layers_conf = [3, 5, 2]
-a = init_params(layers_conf)
-
-
 def relu(x):
     return max(0, x)
 
@@ -37,15 +33,32 @@ def loss(actual, predicted):
     return (predicted - actual) ** 2
 
 
-def backwardPass(layers, backData, grads, lr=1e-4):
+def backwardPass(layers, backData, grads, lr=1e-6):
     for i in range(-1, -(len(layers)+1), -1):
         if i != len(layers):
-            grad = np.multiply(grad, np.heaviside(backData[i+1], 0))
+            grads = np.multiply(grads, np.heaviside(backData[i+1], 0))
 
-        w_grad = backData[i].T @ grad
-        b_grad = np.mean(grad, axis=0)
+        w_grad = backData[i].T @ grads
+        b_grad = np.mean(grads, axis=0)
 
         layers[i][0] -= lr * w_grad
         layers[i][1] -= lr * b_grad
 
-        grad = grad @ layers[i][0].T
+        grads = grads @ layers[i][0].T
+    return layers
+
+
+layers_conf = [3, 5, 2]  # network architecture
+layers = init_params(layers_conf)
+iters = 100
+loss_count = []
+
+for _ in range(iters):
+    prediction, Backdata = forwardPass(data, layers)
+    lossValue = loss(prediction, actual)
+    loss_count.append(lossValue)
+
+    layers = backwardPass(layers, Backdata, grads)
+
+plt.plot((0, len(loss_count), loss_count))  # plotting loss vs iterations
+plt.show()
