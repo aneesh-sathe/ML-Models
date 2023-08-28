c43ecb462b60d88e99aff920869a4883062ef3f2
aneesh-sathe <117112887+aneesh-sathe@users.noreply.github.com>
2023-08-28 23:11:26 +0800

    added init_layers

--- neural_net.py
+++ neural_net.py
@@ -2,7 +2,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# we will create a 3-4-1 neural network architecture
+# we will create a 3-5-2 neural network architecture
 
 
-def init_params():
+def init_params(layers_conf):  # initialise the weight and bias matrices based on the layers
+    layers = []
+    for i in range(1, len(layers_conf)):
+        layers.append([
+            np.random.rand(layers_conf[i-1], layers_conf[i]),
+            np.zeros((1, layers_conf[i]))
+        ])
+    return layers
+
+
+layers_conf = [3, 5, 2]
+a = init_params(layers_conf)
+print(a)
